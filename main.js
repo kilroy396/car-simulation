7606f829f915b93efb6827476ce135a4bb717b0e
kilroy396 <kilroy396@users.noreply.github.com>
2024-01-15 05:02:30 +1100

    scrappy change to main

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ if (localStorage.getItem('bestBrain')) {
 	for (let i = 0; i < cars.length; i++) {
 		cars[i].brain = JSON.parse(localStorage.getItem('bestBrain'));
 		if (i != 0) {
-			NeuralNetwork.mutate(cars[i].brain, 0.4);
+			NeuralNetwork.mutate(cars[i].brain, 0.2);
 		}
 	}
 }
@@ -61,7 +61,6 @@ const track_3 = [
 	new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2),
 	new Car(road.getLaneCenter(0), -300, 30, 50, 'DUMMY', 2),
 	new Car(road.getLaneCenter(2), -300, 30, 50, 'DUMMY', 2),
-	new Car(road.getLaneCenter(0), -500, 30, 50, 'DUMMY', 2),
 	new Car(road.getLaneCenter(1), -500, 30, 50, 'DUMMY', 2),
 	new Car(road.getLaneCenter(0), -700, 30, 50, 'DUMMY', 2),
 	new Car(road.getLaneCenter(2), -700, 30, 50, 'DUMMY', 2),
@@ -98,7 +97,7 @@ const track_3 = [
 	new Car(road.getLaneCenter(1), -4800, 30, 50, 'DUMMY', 2),
 ];
 
-const traffic = track_3;
+const traffic = track_1;
 
 animate();
 
